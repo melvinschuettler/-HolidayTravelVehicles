23c2f1f2b6c1fcee80f4ec20d49c95bd7a9c9112
melvinschuettler <melvin.schuettler@gmail.com>
2025-09-21 16:08:57 +0700

    Create Invoice.java

---
+++ Invoice.java
@@ -0,0 +1,10 @@
+public class Invoice {
+    private String fullCustomerInformation;
+    private String tradeInInformation;
+    private String dealerInstalledOption;
+    private String finalPrice;
+    private String applicationTaxes;
+    private String licenseFees;
+    private String salespersonSignature;
+    private String customerSignature;
+}
