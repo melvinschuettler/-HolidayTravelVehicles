bdffb3ffae5c0027a3f5d90359c389e5bfa0eafd
melvinschuettler <melvin.schuettler@gmail.com>
2025-09-21 16:12:26 +0700

    Update NewVehicle.java
    getter and setter update

--- NewVehicle.java
+++ NewVehicle.java
@@ -1,7 +1,47 @@
 public class NewVehicle {
-private String vehicleSerialNumber;
-private String vehicleName;
-private String vehicleModel;
-private String year;
-private String manufacturer;
+    private String vehicleSerialNumber;
+    private String vehicleName;
+    private String vehicleModel;
+    private String year;
+    private String manufacturer;
+
+    public String getVehicleSerialNumber() {
+        return vehicleSerialNumber;
+    }
+
+    public void setVehicleSerialNumber(String vehicleSerialNumber) {
+        this.vehicleSerialNumber = vehicleSerialNumber;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(String vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
