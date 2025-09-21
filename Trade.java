a79cb45555ae4649d0bf68d0bd62eeb0102aba40
melvinschuettler <melvin.schuettler@gmail.com>
2025-09-21 16:09:14 +0700

    Create Trade.java

---
+++ Trade.java
@@ -0,0 +1,7 @@
+public class Trade {
+    private Customer customer;
+    private Salesperson salesperson;
+    private TradeVehicle tradeVehicle;
+    private NewVehicle newVehicle;
+    private Invoice invoice;
+}
